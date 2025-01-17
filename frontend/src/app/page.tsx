c0c3bb4b0c933fd13110f1d97eaeb5c40db9e784
Roger <rogerbambinetti@hotmail.com>
2025-01-17 12:16:42 -0300

    add loading state

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,76 +1,48 @@
 'use client'
 
-import Image from "next/image";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import axios from "axios";
+import { TailSpin } from 'react-loader-spinner';
 
 const fileTypes = ["M4A"];
 
 export default function Home() {
 
-  const [file, setFile] = useState(null);
-  const handleChange = async (file: any) => {
+  const [isConvertingFiles, setIsConvertingFiles] = useState(false);
 
-    const form = new FormData();
-    form.append('file', file);
+  const handleChange = async (file: File) => {
+    try {
+      const form = new FormData();
+      form.append('file', file);
 
-    const { data } = await axios.post("http://localhost:3030/files/convert", form);
-    window.open(`http://localhost:3030/files/download/${data.fileName}`);
+      setIsConvertingFiles(true);
+      const { data } = await axios.post("http://localhost:3030/files/convert", form);
+      setIsConvertingFiles(false);
+
+      window.location.href = `http://localhost:3030/files/download/${data.fileName}`;
+    } catch (error) {
+      console.error("Error converting file:", error);
+      setIsConvertingFiles(false);
+    }
   };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+
+        {isConvertingFiles ?
+          <TailSpin
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="loading"
+          />
+          :
+          <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+        }
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
