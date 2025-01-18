df0cbb63dc6d6df2356878c28eb6a787bfc727ce
Roger <rogerbambinetti@hotmail.com>
2025-01-18 17:35:11 -0300

    adding install script

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -56,6 +56,7 @@ export default function Home() {
           defaultValue={selectedOption}
           onChange={(newValue) => setSelectedOption(newValue as SelectOption)}
           options={options}
+          className="w-80"
         />
 
         {isConvertingFiles ?
