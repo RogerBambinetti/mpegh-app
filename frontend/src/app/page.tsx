889bc6eff0315bed3456b6b21f7f2da6786a6eae
Roger <rogerbambinetti@hotmail.com>
2025-03-10 11:19:28 -0300

    fix load spinner align

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
 
         <Select
           defaultValue={selectedOption}
@@ -66,7 +66,7 @@ export default function Home() {
           <TailSpin
             height="80"
             width="80"
-            color="#4fa94d"
+            color="purple"
             ariaLabel="loading"
           />
           :
